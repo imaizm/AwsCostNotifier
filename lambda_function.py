abac22270f69398c4cea6e9c27b08ac47051037c
imaizm <imaizm@gmail.com>
2017-07-03 15:09:08 +0900

    Slack→HipChatへ変更・lambdaロールを修正・lambda関連の中間ファイルを.gitignoreへ追加

--- lambda_function.py
+++ lambda_function.py
@@ -11,11 +11,12 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Slack の設定
-SLACK_POST_URL = os.environ['slackPostURL']
-SLACK_CHANNEL = os.environ['slackChannel']
+# HipChat の設定
+HIPCHAT_ROOM_ID = os.environ['hipchatRoomId']
+HIPCHAT_API_TOKEN = os.environ['hipchatApiToken']
+HIPCHAT_POST_URL = "https://api.hipchat.com/v2/room/" + HIPCHAT_ROOM_ID + "/notification?auth_token=" + HIPCHAT_API_TOKEN
 
-response = boto3.client('cloudwatch', region_name='us-east-1')
+response = boto3.client('cloudwatch', region_name='us-west-2')
 
 get_metric_statistics = response.get_metric_statistics(
     Namespace='AWS/Billing',
@@ -35,30 +36,29 @@ cost = get_metric_statistics['Datapoints'][0]['Maximum']
 date = get_metric_statistics['Datapoints'][0]['Timestamp'].strftime('%Y年%m月%d日')
 
 def build_message(cost):
-    if float(cost) >= 10.0:
-        color = "#ff0000" #red
-    elif float(cost) > 0.0:
-        color = "warning" #yellow
-    else:
-        color = "good"    #green
+#    if float(cost) >= 10.0:
+#        color = "#ff0000" #red
+#    elif float(cost) > 0.0:
+#        color = "warning" #yellow
+#    else:
+#        color = "good"    #green
 
-    text = "%sまでのAWSの料金は、$%sです。" % (date, cost)
+    message = "@all %sまでのAWSの料金は、$%sです。" % (date, cost)
 
-    atachements = {"text":text,"color":color}
+    atachements = {
+        "from":"AwsCostNotifier",
+        "notify":"true",
+        "message_format":"text",
+        "message":message
+    }
     return atachements
 
 def lambda_handler(event, context):
-    content = build_message(cost)
+    json_body = build_message(cost)
 
-    # SlackにPOSTする内容をセット
-    slack_message = {
-        'channel': SLACK_CHANNEL,
-        "attachments": [content],
-    }
-
-    # SlackにPOST
+    # HipChatにPOST
     try:
-        req = requests.post(SLACK_POST_URL, data=json.dumps(slack_message))
-        logger.info("Message posted to %s", slack_message['channel'])
+        req = requests.post(HIPCHAT_POST_URL, data=json.dumps(json_body))
+        logger.info("Message posted to %s", HIPCHAT_ROOM_ID)
     except requests.exceptions.RequestException as e:
         logger.error("Request failed: %s", e)
