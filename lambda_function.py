8d2cff14e5bed54aac8e84789356ffe610d52d05
imaizm <imaizm@gmail.com>
2017-07-05 17:52:38 +0900

    請求情報はus-east-1のみに存在する点に対応。その他、HipChatのAPI Call設定など

--- lambda_function.py
+++ lambda_function.py
@@ -16,26 +16,7 @@ HIPCHAT_ROOM_ID = os.environ['hipchatRoomId']
 HIPCHAT_API_TOKEN = os.environ['hipchatApiToken']
 HIPCHAT_POST_URL = "https://api.hipchat.com/v2/room/" + HIPCHAT_ROOM_ID + "/notification?auth_token=" + HIPCHAT_API_TOKEN
 
-response = boto3.client('cloudwatch', region_name='us-west-2')
-
-get_metric_statistics = response.get_metric_statistics(
-    Namespace='AWS/Billing',
-    MetricName='EstimatedCharges',
-    Dimensions=[
-        {
-            'Name': 'Currency',
-            'Value': 'USD'
-        }
-    ],
-    StartTime=datetime.datetime.today() - datetime.timedelta(days=1),
-    EndTime=datetime.datetime.today(),
-    Period=86400,
-    Statistics=['Maximum'])
-
-cost = get_metric_statistics['Datapoints'][0]['Maximum']
-date = get_metric_statistics['Datapoints'][0]['Timestamp'].strftime('%Y年%m月%d日')
-
-def build_message(cost):
+def build_message(cost, date):
 #    if float(cost) >= 10.0:
 #        color = "#ff0000" #red
 #    elif float(cost) > 0.0:
@@ -51,14 +32,37 @@ def build_message(cost):
         "message_format":"text",
         "message":message
     }
+
     return atachements
 
 def lambda_handler(event, context):
-    json_body = build_message(cost)
+    response = boto3.client('cloudwatch', region_name='us-east-1')
+
+    get_metric_statistics = response.get_metric_statistics(
+        Namespace='AWS/Billing',
+        MetricName='EstimatedCharges',
+        Dimensions=[
+            {
+                'Name': 'Currency',
+                'Value': 'USD'
+            }
+        ],
+        StartTime=datetime.datetime.today() - datetime.timedelta(days=1),
+        EndTime=datetime.datetime.today(),
+        Period=86400,
+        Statistics=['Maximum'])
+
+    logger.info("%s", str(get_metric_statistics))
+
+    cost = get_metric_statistics['Datapoints'][0]['Maximum']
+    date = get_metric_statistics['Datapoints'][0]['Timestamp'].strftime('%Y年%m月%d日')
+
+    json_body = build_message(cost, date)
 
     # HipChatにPOST
     try:
-        req = requests.post(HIPCHAT_POST_URL, data=json.dumps(json_body))
-        logger.info("Message posted to %s", HIPCHAT_ROOM_ID)
+        req = requests.post(HIPCHAT_POST_URL, data=json.dumps(json_body), headers={'Content-Type': 'application/json'})
+        logger.info("Message posted to %s", HIPCHAT_POST_URL)
+        logger.info("%s", json.dumps(json_body))
     except requests.exceptions.RequestException as e:
         logger.error("Request failed: %s", e)
