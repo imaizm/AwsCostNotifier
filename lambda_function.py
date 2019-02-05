afc46cdb077e7ab4897c8ba688d4b8fc5994d751
imaizm <imaizm@gmail.com>
2019-02-05 20:01:43 +0900

    hipchat -> slack

--- lambda_function.py
+++ lambda_function.py
@@ -11,29 +11,31 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# Slack の設定
+SLACK_POST_URL = os.environ['slackPostURL']
+SLACK_CHANNEL = os.environ['slackChannel']
+
 # HipChat の設定
-HIPCHAT_ROOM_ID = os.environ['hipchatRoomId']
-HIPCHAT_API_TOKEN = os.environ['hipchatApiToken']
-HIPCHAT_POST_URL = "https://api.hipchat.com/v2/room/" + HIPCHAT_ROOM_ID + "/notification?auth_token=" + HIPCHAT_API_TOKEN
+#HIPCHAT_ROOM_ID = os.environ['hipchatRoomId']
+#HIPCHAT_API_TOKEN = os.environ['hipchatApiToken']
+#HIPCHAT_POST_URL = "https://api.hipchat.com/v2/room/" + HIPCHAT_ROOM_ID + "/notification?auth_token=" + HIPCHAT_API_TOKEN
 
 def build_message(cost, date):
-#    if float(cost) >= 10.0:
-#        color = "#ff0000" #red
-#    elif float(cost) > 0.0:
-#        color = "warning" #yellow
-#    else:
-#        color = "good"    #green
+    if float(cost) >= 10.0:
+        color = "#ff0000" #red
+    elif float(cost) > 0.0:
+        color = "warning" #yellow
+    else:
+        color = "good"    #green
 
-    message = "@all %sまでのAWSの料金は、$%sです。" % (date, cost)
+    message = "@channel %sまでのAWSの料金は、$%sです。" % (date, cost)
 
-    atachements = {
-        "from":"AwsCostNotifier",
-        "notify":"true",
-        "message_format":"text",
-        "message":message
+    attachements = {
+        "color":color,
+        "text":message
     }
 
-    return atachements
+    return attachements
 
 def lambda_handler(event, context):
     response = boto3.client('cloudwatch', region_name='us-east-1')
@@ -59,10 +61,17 @@ def lambda_handler(event, context):
 
     json_body = build_message(cost, date)
 
+    slack_message = {
+        'channel': SLACK_CHANNEL,
+        "attachments": [json_body]
+    }
+
     # HipChatにPOST
     try:
-        req = requests.post(HIPCHAT_POST_URL, data=json.dumps(json_body), headers={'Content-Type': 'application/json'})
-        logger.info("Message posted to %s", HIPCHAT_POST_URL)
-        logger.info("%s", json.dumps(json_body))
+    #    req = requests.post(HIPCHAT_POST_URL, data=json.dumps(json_body), headers={'Content-Type': 'application/json'})
+    #    logger.info("Message posted to %s", HIPCHAT_POST_URL)
+        req = requests.post(SLACK_POST_URL, data=json.dumps(slack_message), headers={'Content-Type': 'application/json'})
+        logger.info("Message posted to %s", SLACK_POST_URL)
+        logger.info("%s", json.dumps(slack_message))
     except requests.exceptions.RequestException as e:
         logger.error("Request failed: %s", e)
